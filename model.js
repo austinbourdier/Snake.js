7f8ec2de48586bb05f6a5f34f275d6d7ca99f732
Austin Bourdier <austinbourdier@gmail.com>
2014-08-03 13:55:28 -0700

    fixed bug where snake was randomly growing larger without having ate the food, next: tests, then figure out how to make the snake a line instead of a box

--- model.js
+++ model.js
@@ -9,7 +9,7 @@ var Model = {
     } else if (event.which == 40){
       Controller.moveSnake("down");
     }
-    if($('.snake').css("top").slice(0,-2) <= $('.food').css("top").slice(0,-2) && $('.food').css("top").slice(0,-2) <= (eval($('.snake').css("height").slice(0,-2)) + eval($('.food').css("top").slice(0,-2))) && $('.snake').css("left").slice(0,-2) <= $('.food').css("left").slice(0,-2) && $('.food').css("left").slice(0,-2) <= eval($('.snake').css("width").slice(0,-2)) + eval($('.food').css("left").slice(0,-2))){
+    if($('.snake').css("top").slice(0,-2) <= $('.food').css("top").slice(0,-2) && $('.food').css("top").slice(0,-2) <= (eval($('.snake').css("height").slice(0,-2)) + eval($('.snake').css("top").slice(0,-2))) && $('.snake').css("left").slice(0,-2) <= $('.food').css("left").slice(0,-2) && $('.food').css("left").slice(0,-2) <= eval($('.snake').css("width").slice(0,-2)) + eval($('.snake').css("left").slice(0,-2))){
       Controller.moveFood();
       Model.increaseSnakeLength();
     }
